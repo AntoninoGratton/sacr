36a1f5bba17f0ee684beda33c17469caa3af9e14
AntoninoGratton <antonino.gratton@gmail.com>
2021-12-10 04:58:00 -0300

    Agrego un csrf_exempt

--- sacrApi/sacrApi/views.py
+++ sacrApi/sacrApi/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from sacrApi.internos import *
 import json
 
@@ -6,6 +7,8 @@ def prueba(request):
     if request.method == 'GET':
         return HttpResponse(json_response("Holass desde el back"), status=200, content_type="application/json")
 
+
+@csrf_exempt
 def pruebaPost(request):
     if request.method == 'POST':
         post_data = json.loads(request.body.decode("UTF-8"))
