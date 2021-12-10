a3f29209cdfb6d95825489c697496b66e6b89e93
AntoninoGratton <antonino.gratton@gmail.com>
2021-12-10 04:40:10 -0300

    Corrijo un error en el backend que no dejaba funcionar el ejemplo de POST

--- sacrApi/sacrApi/views.py
+++ sacrApi/sacrApi/views.py
@@ -7,7 +7,7 @@ def prueba(request):
         return HttpResponse(json_response("Holass desde el back"), status=200, content_type="application/json")
 
 def pruebaPost(request):
-    if request.method = 'POST':
+    if request.method == 'POST':
         post_data = json.loads(request.body.decode("UTF-8"))
         print(post_data)
         return HttpResponse(json_response("Correcto"), status=200, content_type="application/json")
